f221c635df28c542e8caa8fb7bfe62ed1efe5d09
Daniel O'Hare <71663427+dohare-byu@users.noreply.github.com>
2021-02-17 19:34:08 -0700

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment5.Models
+{
+    public class Book
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Publisher { get; set; }
+        [RegularExpression(@"^(?=:\D*\d){3}(?:(?:\D*\d){10})?$)[\d-]+$",
+            ErrorMessage ="ISBN is not in the correct format.")]
+        public string ISBN { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public double Price { get; set; }
+    }
+}
---
+++ Models/EFAssignment5Repository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment5.Models
+{
+    public class EFAssignment5Repository : IAssignment5Repository
+    {
+        private Assignment5DBContext _context;
+        public EFAssignment5Repository (Assignment5DBContext context)
+        {
+            _context = context;
+        }
+        public IQueryable<Book> Books => _context.Books;
+    }
+}
